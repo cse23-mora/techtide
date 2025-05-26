9b667f83230458ade5e017269dbce02200b2b636
Suhas Dissanayake <suhasdissa@gmail.com>
2025-05-26 20:14:41 +0530

    Update style

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'CSE 23 - Computer Science & Engineering - Batch 23, University of Moratuwa';
-export const SITE_DESCRIPTION = 'CSE 23 - is the official website of the Computer Science & Engineering - Batch 23, University of Moratuwa. This site is dedicated to providing information and resources for the students of CSE 23.';
+export const SITE_TITLE = 'Techtide by CSE 23';
+export const SITE_DESCRIPTION = 'Techtide is a blog where students of CSE 23 batch share their experience about new teechnology as a weekly article series';
